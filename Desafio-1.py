2bca8931626270de25fc9cfa6a2da0cf4e8c6ccf
K0nz <99678793+K0nz@users.noreply.github.com>
2022-02-14 20:06:11 -0300

    Update Desafio-1.py

--- Desafio-1.py
+++ Desafio-1.py
@@ -1,8 +1,10 @@
 #Programa-resposta para a primeira questão do desafio de programação
 
+#Input do número de linhas a ser impresso
 print("Insira seu número:")
 n = int(input())
 
+#Impressão da quantidade de '*' por linha equivalente à linha
 x = 1
 print("Saída:")
 while x <= n:
